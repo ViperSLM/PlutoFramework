87308a9c45c02e6a33ccdff7fb643789d272c4cf
ViperSLM <61650364+ViperSLM@users.noreply.github.com>
2025-05-06 18:28:50 +0930

    Main function easier to implement
    How to use:
    
    PLUTO_MAIN {
      PLUTO_SETUPARGS(); // New macro
      App app;
      return app.Run(&myArgc, myArgv, myHInstance);
    }

--- src/c_main.h
+++ src/c_main.h
@@ -15,11 +15,35 @@
 // Windows GUI subsystem (WinMain)
 #if defined(_WIN32) && defined(_PLUTO_WINMAIN_)
 #include <windows.h>
-#define PLUTO_MAIN int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow)
+#define PLUTO_MAIN     \
+  int CALLBACK WinMain \
+  (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow)
+
+#define PLUTO_HINSTANCE() \
+  void *myHInstance = reinterpret_cast<void*>(hInstance)
+
+#define PLUTO_MAINARGS() \
+  int myArgc = __argc;   \
+  char **myArgv = __argv
+
 #else
 
+#define PLUTO_HINSTANCE() \
+  void *myHInstance = nullptr
+
+#define PLUTO_MAINARGS() \
+  int myArgc = argc;     \
+  char **myArgv = argv   \
+
 // Traditional main function
-#define PLUTO_MAIN int main(int argc, char** argv)
+#define PLUTO_MAIN \
+  int main(int argc, char** argv)
 
 #endif
+
+// Setup argument variables
+#define PLUTO_SETUPARGS() \
+  PLUTO_HINSTANCE();      \
+  PLUTO_MAINARGS()        \
+
 #endif // Include guard
